0d00aef4325ca532a226535134dc9cc13b08cfbd
Vigo Luthjens <2101466luthjens@zuyd.nl>
2024-05-27 11:06:31 +0200

    Added result overview page

---
+++ src/ResultOverview.jsx
@@ -0,0 +1,14 @@
+// src/MyPage.js
+import React from 'react';
+import './ResultOverview.css'; // Import the CSS file
+
+function MyPage() {
+  return (
+    <div className="my-page">
+      <h1>Welcome to My Page</h1>
+      <p>This is a new React page with CSS styling.</p>
+    </div>
+  );
+}
+
+export default MyPage;
