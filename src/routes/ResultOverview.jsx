3505cb614c0f6f5a31701cf5c48a0a9620e1c6df
Vigo Luthjens <2101466luthjens@zuyd.nl>
2024-05-28 10:28:43 +0200

    ResultOverview added

--- src/routes/ResultOverview.jsx
+++ src/routes/ResultOverview.jsx
@@ -1,15 +1,50 @@
 // src/MyPage.js
 import React from 'react';
-import './ResultOverview.css'; // Import the CSS file
+import '../App.css'; // Import the CSS file
 import Navbar from '../components/Navbar'; //double period to go back one directory
 
 function ResultOverview() {
+  const DataRow = ({ type, date }) => (
+    <tr onClick={() => window.alert('Row clicked!')}>
+      <td className="text-cell"><div className="rounded-left">{type}</div></td>
+      <td className="text-cell"><div className="rounded-right">{date}</div></td>
+    </tr>
+  );
+
+  const DataTable = ({ data }) => (
+    <>
+      <table className='table_2'>
+        <thead>
+          <tr>
+            <th className="th-header-left"><div className="header-rounded-left header-item">Type</div></th>
+            <th className="th-header-right"><div className="header-rounded-right header-item">Date</div></th>
+          </tr>
+        </thead>
+      </table>
+
+      <div className="scrollable-table">
+        <table className='table_2'>
+          <tbody>
+            {data.map((row, index) => <DataRow key={index} {...row} />)}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+  const data = [
+    { type: 'Myometrie', date: '19/03/2024' },
+    { type: 'Radiologie', date: '01/07/2022' },
+    { type: 'Myometrie', date: '27/11/2020' },
+    { type: 'Myometrie', date: '09/10/2019' },
+  ];
+
+
   return (
-     <div className="ResultOverview">
+    <>
       <Navbar />
-      <h1>Welcome to My Page</h1>
-      <p>This is a new React page with CSS styling.</p>
-    </div>
+      <h1>Resultatenoverzicht</h1>
+      <DataTable data={data} />
+    </>
   );
 }
 
