54c3084317833b2adb4be655986f9de868273cd4
Vigo Luthjens <2101466luthjens@zuyd.nl>
2024-05-28 10:28:43 +0200

    ResultOverview added

--- src/routes/ResultOverview.jsx
+++ src/routes/ResultOverview.jsx
@@ -1,54 +1,15 @@
-import React, { useState, useEffect } from 'react';
+// src/MyPage.js
+import React from 'react';
 import '../App.css'; // Import the CSS file
-import Navbar from '../components/Navbar'; // Double period to go back one directory
-import { useNavigate } from 'react-router-dom';
-import TopPage from '../components/TopPage';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-
+import Navbar from '../components/Navbar'; //double period to go back one directory
 
 function ResultOverview() {
-  const { patientId } = useParams();
-  const imageSrc = '../src/assets/kid_1.png';
-  const patientName = 'John Doe';
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async (patientId) => {
-      try {
-        const response = await axios.get(`http://localhost:5000/patients/${patientId}/get_results`);
-        setData(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData(patientId);
-  }, [patientId]);
-
-  const DataRow = ({ Type, Date }) => {
-    const navigate = useNavigate();
-
-    const handleClick = () => {
-      if (Type === 'Myometrie') {
-        navigate(`/myometriepage/${patientId}`);
-      } else if (Type === 'Radiologie') {
-        navigate(`/radiologypage/${patientId}`);
-      } else {
-        window.alert(`Row clicked: ${Type}`);
-      }
-    };
-
-    // Format the date to display only the date part
-    const formattedDate = new window.Date(Date).toLocaleDateString('en-GB')
-
-    return (
-      <tr onClick={handleClick}>
-        <td className="text-cell"><div className="rounded-left">{Type}</div></td>
-        <td className="text-cell"><div className="rounded-right">{formattedDate}</div></td>
-      </tr>
-    );
-  };
+  const DataRow = ({ type, date }) => (
+    <tr onClick={() => window.alert('Row clicked!')}>
+      <td className="text-cell"><div className="rounded-left">{type}</div></td>
+      <td className="text-cell"><div className="rounded-right">{date}</div></td>
+    </tr>
+  );
 
   const DataTable = ({ data }) => (
     <>
@@ -70,14 +31,20 @@ function ResultOverview() {
       </div>
     </>
   );
+  const data = [
+    { type: 'Myometrie', date: '19/03/2024' },
+    { type: 'Radiologie', date: '01/07/2022' },
+    { type: 'Myometrie', date: '27/11/2020' },
+    { type: 'Myometrie', date: '09/10/2019' },
+  ];
+
 
   return (
     <>
+    <>
       <Navbar />
-      <TopPage headerName="Patient" patientId={patientId} imageSrc={imageSrc} />
-      <div className="content">
-        <DataTable data={data} />
-      </div>
+      <h1>Resultatenoverzicht</h1>
+      <DataTable data={data} />
     </>
   );
 }
