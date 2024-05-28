a1a10270ed471134868630b021cd1496a881038f
RealGammaNL <max@brosky.net>
2024-05-28 10:27:55 +0200

    Added Patientviewpage

--- src/main.jsx
+++ src/main.jsx
@@ -10,29 +10,14 @@ import './Login/App.css';
 import PatientOverview from './routes/PatientOverview.jsx';
 import ResultOverview from './routes/ResultOverview.jsx';
 import PatientView from './routes/PatientView.jsx';
-import MyometriePage from './routes/MyometriePage.jsx';
-import EssayPage from './routes/EssayPage.jsx';
-import RadiologyPage from './routes/RadiologyPage.jsx';
-import PatientSettingsPage from './routes/PatientSettingsPage.jsx';
-import PatientList from './components/PatientList.jsx';
-import Kalender from './routes/Kalender.jsx';
-
 
 
 const router = createBrowserRouter([
     { path: "/", element: <App/> },
     { path: "/patientoverview", element: <PatientOverview /> },
-    { path: "/patientview/:patientId", element: <PatientView /> },
-    { path: "/resultoverview/:patientId", element: <ResultOverview /> },
-    { path: "/myometriepage/:patientId", element: <MyometriePage /> },
-    { path: "/essaypage/:patientId", element: <EssayPage /> },
-    { path: "/radiologypage/:patientId", element: <RadiologyPage /> },
-    { path: "/patientsettingspage/:patientId", element: <PatientSettingsPage /> },
-    { path: "/patientlist", element: <PatientList /> },
-    { path: "/kalender", element: <Kalender /> },
-    { path: "/", element: <App /> },
-    { path: "/register", element: <Register /> },
-    { path: "/forgot-password", element: <ForgotPassword /> },
+    { path: "/patientview", element: <PatientView /> },
+    { path: "/resultoverview", element: <ResultOverview /> },
+
 ]);
 
 
--- src/routes/PatientView.jsx
+++ src/routes/PatientView.jsx
@@ -1,59 +1,15 @@
+// src/MyPage.js
 import Navbar from '../components/Navbar'; //double period to go back one directory
-import App from '../App';
-import { Link } from 'react-router-dom';
-import TopPage from '../components/TopPage';
-import { useParams } from 'react-router-dom';
+import '../App.css';
 
 function PatientView() {
-    const { patientId } = useParams();
-    const imageSrc = '../src/assets/kid_1.png';
-    const patientName = 'John Doe';
-    const phoneNumber = '0612345678';
-    const mail = 'John_doe@gmail.com';
-    const diagnosis = 'JDM';
-
-
-    return (
-        <>
-            <Navbar />
-            <div className="PatientView">
-                <TopPage headerName="Patient" patientId={patientId} imageSrc={imageSrc} />
-                <div className='PageBottom'>
-                    <div className="ResultsBlock Block card">
-                        <p>Resultaten</p>
-                        <div className="DataBlockData">
-                            {Array.from({ length: 3 }, (_, i) => (
-                                <div className="textual-data-row" key={i + 1}>
-                                    <p>{i + 1}: Result {i + 1}</p>
-                                </div>
-                            ))}
-                        </div>
-                        <Link to={`/resultoverview/${patientId}`} className="resultoverview-button"><button>Zie alles</button></Link>
-                    </div>
-                    <div className="EssayBlock Block card">
-                        <p>Verslagen</p>
-                        <div className="DataBlockData">
-                            {Array.from({ length: 3 }, (_, i) => (
-                                <div className="textual-data-row" key={i + 1}>
-                                    <p>{i + 1}: Verslag {i + 1}</p>
-                                </div>
-                            ))}
-                        </div>
-                        <Link to={`/essaypage/${patientId}`}><button>Zie alles</button></Link>
-                    </div>
-                    <div className="DataBlock Block card">
-                        <p>Gegevens</p>
-                        <div className="DataBlockData">
-                            <p>Nummer: {phoneNumber}</p>
-                            <p>Mail: {mail}</p>
-                            <p>Diagnose: {diagnosis}</p>
-                        </div>
-                        <Link to={`/patientsettingspage/${patientId}`}><button>Zie alles</button></Link>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+     <div className="PatientView">
+      <Navbar />
+      <h1>Patient</h1>
+      <p>This is a new React page with CSS styling.</p>
+    </div>
+  );
 }
 
 export default PatientView;
