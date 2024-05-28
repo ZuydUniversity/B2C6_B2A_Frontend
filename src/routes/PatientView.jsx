b81ab1b262ef72e3940d81aa3ed5a029b583fe21
RealGammaNL <max@brosky.net>
2024-05-28 11:28:03 +0200

    Added things to PatientView

--- src/routes/PatientView.jsx
+++ src/routes/PatientView.jsx
@@ -1,13 +1,50 @@
-// src/MyPage.js
 import Navbar from '../components/Navbar'; //double period to go back one directory
-import '../App.css';
+import App from '../App';
 
 function PatientView() {
+  const imageSrc = '../src/assets/kid_1.png';
+  const patientName = 'John Doe';
+
+
   return (
-     <div className="PatientView">
+    <div className="PatientView">
       <Navbar />
-      <h1>Patient</h1>
-      <p>This is a new React page with CSS styling.</p>
+      <div className="PageTop">
+        <h1>{patientName}</h1>
+        <img className="patient-image" src={imageSrc}></img>
+      </div>
+      <div className='PageBottom'>
+        <div className="ResultsBlock Block">
+          <p>Resultaten</p>
+          {Array.from({ length: 3 }, (_, i) => (
+            <div className="textual-data-row" key={i}>
+              <p>{i}</p>
+              <p>result {i}</p>
+            </div>
+          ))}
+          <button>Zie alles</button>
+        </div>
+        <div className="EssayBlock Block">
+          <p>Resultaten</p>
+          {Array.from({ length: 3 }, (_, i) => (
+            <div className="textual-data-row" key={i}>
+              <p>{i}</p>
+              <p>result {i}</p>
+            </div>
+          ))}
+          <button>Zie alles</button>
+        </div>
+        <div className="DataBlock Block">
+          <p>Resultaten</p>
+          {Array.from({ length: 3 }, (_, i) => (
+            <div className="textual-data-row" key={i}>
+              <p>{i}</p>
+              <p>result {i}</p>
+            </div>
+          ))}
+          <button>Zie alles</button>
+        </div>
+      </div>
     </div>
   );
 }
