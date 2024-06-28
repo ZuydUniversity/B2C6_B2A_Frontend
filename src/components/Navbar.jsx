f9d4822eea2f780d7b61b48ef0c6f1a76aaaca8a
lckssng <56154586+lckssng@users.noreply.github.com>
2024-06-28 13:04:11 +0200

    Navbar gefixt met de nieuwe javascript voor de collapsable button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,37 +18,41 @@ const CustomNavbar = () => {
     };
 
     return (
-        <Navbar bg="light" expand="lg" className="border-bottom">
-            <Navbar.Toggle aria-controls="navbarSupportedContent" />
-            <Navbar.Collapse id="navbarSupportedContent">
-                <Nav className="me-auto">
-                    <Nav.Link as={Link} to="/DoctorDashboard" className="btn btn-outline-primary"><i className="bi bi-house-fill"></i> Home</Nav.Link>
-                    <Nav.Link as={Link} to="/PatientOverview" className="btn btn-outline-primary"><i className="bi bi-people-fill"></i> Patiëntenoverzicht</Nav.Link>
-                    <Nav.Link as={Link} to="/Kalender" className="btn btn-outline-primary"><i className="bi bi-calendar-event-fill"></i> Kalender</Nav.Link>
-                </Nav>
-                <Form className="d-flex" onSubmit={handleSearchSubmit}>
-                    <FormControl
-                        type="search"
-                        placeholder="Zoeken..."
-                        className="mr-1"
-                        aria-label="Zoeken"
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                    />
-                    <Button type="submit"><i className="bi bi-search"></i></Button>
-                </Form>
-                <Dropdown>
-                    <Dropdown.Toggle variant="outline-primary" id="dropdownMenuButton">
-                        <i className="bi bi-person-circle"></i> Profiel
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        <Dropdown.Item as={Link} to="/"><i className="bi bi-person-bounding-box"></i> Profielweergave</Dropdown.Item>
-                        <Dropdown.Item as={Link} to="/ProfilePage"><i className="bi bi-person-fill-gear"></i> Profielinstellingen</Dropdown.Item>
-                        <Dropdown.Divider />
-                        <Dropdown.Item as={Link} to="/"><i className="bi bi-box-arrow-left"></i> Uitloggen</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-            </Navbar.Collapse>
+        <Navbar expand="lg" className="border-bottom">
+            <div className="navbar-container">
+                <Navbar.Toggle aria-controls="navbarSupportedContent" />
+                <Navbar.Collapse id="navbarSupportedContent">
+                    <Nav className="me-auto">
+                        <Button as={Link} to="/DoctorDashboard" className="btn btn-outline-primary"><i className="bi bi-house-fill"></i> Home</Button>
+                        <Button as={Link} to="/PatientOverview" className="btn btn-outline-primary"><i className="bi bi-people-fill"></i> Patiëntenoverzicht</Button>
+                        <Button as={Link} to="/Kalender" className="btn btn-outline-primary"><i className="bi bi-calendar-event-fill"></i> Kalender</Button>
+                    </Nav>
+                    <Nav>
+                        <Form className="d-flex" onSubmit={handleSearchSubmit}>
+                            <FormControl
+                                type="search"
+                                placeholder="Zoeken..."
+                                className="form-control input-border-navbar mr-1"
+                                aria-label="Zoeken"
+                                value={searchQuery}
+                                onChange={(e) => setSearchQuery(e.target.value)}
+                            />
+                            <Button variant="outline-primary" type="submit"><i className="bi bi-search"></i></Button>
+                        </Form>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="outline-primary" id="dropdownMenuButton">
+                                <i className="bi bi-person-circle"></i> Profiel
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item as={Link} to="/"><i className="bi bi-person-bounding-box"></i> Profielweergave</Dropdown.Item>
+                                <Dropdown.Item as={Link} to="/ProfilePage"><i className="bi bi-person-fill-gear"></i> Profielinstellingen</Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item as={Link} to="/"><i className="bi bi-box-arrow-left"></i> Uitloggen</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </div>
         </Navbar>
     );
 }
