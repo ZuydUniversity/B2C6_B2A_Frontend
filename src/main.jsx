006934ea9648b70191aef5d0054fe0148d7eeeb6
lckssng <56154586+lckssng@users.noreply.github.com>
2024-06-28 11:41:15 +0200

    404 page gemaakt en routes ernaar toe geleidt

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
 import Register from './routes/register.jsx';
 import ForgotPassword from './routes/forgotpassword.jsx';
 import PatientOverview from './routes/PatientOverview.jsx';
@@ -14,15 +14,14 @@ import PatientSettingsPage from './routes/PatientSettingsPage.jsx';
 import PatientList from './components/PatientList.jsx';
 import Kalender from './routes/Kalender.jsx';
 import PatientDashboard from './routes/PatientDashboard.jsx';
-import DocMenuTest from './menuTests/doctormenu.jsx'
-import PatMenuTest from './menuTests/patientmenu.jsx'
-import ResMenuTest from './menuTests/researchermenu.jsx'
+import DocMenuTest from './menuTests/doctormenu.jsx';
+import PatMenuTest from './menuTests/patientmenu.jsx';
+import ResMenuTest from './menuTests/researchermenu.jsx';
 import AdminMenu from './menuTests/adminmenu.jsx';
 import ResetPassword from './routes/resetpassword.jsx';
 import ProfilePage from './routes/ProfilePage.jsx';
 import DoctorDashboard from './routes/Home.jsx';
-
-
+import PageNotFound from './routes/404Page.jsx'; // Import the 404Page component
 
 const router = createBrowserRouter([
     { path: "/", element: <App/> },
@@ -46,23 +45,12 @@ const router = createBrowserRouter([
     { path: "/resmenu", element: <ResMenuTest /> },
     { path: "/adminmenu", element: <AdminMenu />},
     { path: "/reset-password/:token", element: <ResetPassword /> },
-    { path: "/profilepage/", element: <ProfilePage /> }
+    { path: "/profilepage/", element: <ProfilePage /> },
+    { path: "*", element: <PageNotFound /> } // Add this line to catch all undefined routes
 ]);
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
         <RouterProvider router={router} />
    </React.StrictMode>,
-)
-
-//import React from 'react';
-//import ReactDOM from 'react-dom/client';
-//import ResultOverview from './ResultOverview.jsx'; // Import the new component
-//import './index.css';
-
-//ReactDOM.createRoot(document.getElementById('root')).render(
-//  <React.StrictMode>
-//    <ResultOverview /> {/* Render the new component */}
-//  </React.StrictMode>,
-//);
+);
