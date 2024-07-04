7ca22a0d7d571f6a5791e08693be9e58c429b525
Marc Derks <2307439derks@zuyd.nl>
2024-07-04 15:55:42 +0200

    We linting

--- src/routes/ProfilePage.jsx
+++ src/routes/ProfilePage.jsx
@@ -54,7 +54,7 @@ const Settings = () => {
   }, [navigate]); 
 
   useEffect(() => {
-    const fetchUser = async (id) => {
+    const fetchUser = async () => {
       try {
         const response = await fetch(`http://localhost:5000/get_user/${userId}`);
         if (!response.ok) {
