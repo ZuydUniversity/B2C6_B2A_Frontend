14030c43167d651959280c021952a52d1b029ef4
Hedgehog-From-Outerspace <100069852+Hedgehog-From-Outerspace@users.noreply.github.com>
2024-07-04 21:24:38 +0200

    test adding server details on this end

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 3000,      // Use port 3000
+    strictPort: true,
+    cors: true
+  },
   test: {
     environment: 'jsdom',
     globals: true,
