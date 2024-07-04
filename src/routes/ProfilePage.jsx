10795aaee10cc01a139bcecec231b7b13d920e3d
Marc Derks <2307439derks@zuyd.nl>
2024-07-04 14:02:25 +0200

    Dis mijn eigen branch laat me met routes

--- src/routes/ProfilePage.jsx
+++ src/routes/ProfilePage.jsx
@@ -13,11 +13,53 @@ const Settings = ({ userId }) => {
     phone: '',
   });
   const [isEditingProfile, setIsEditingProfile] = useState(false);
+  const [UserId, setUserId] = useState();
 
   useEffect(() => {
+    async function GetAccountInfo(){
+      try {
+          const authToken = Cookies.get('auth_token');
+          if (!authToken) {
+              throw new Error('Not authenticated');
+          }
+          const response = await fetch('http://127.0.0.1:5000/get_account_info', {
+              method: 'POST', 
+              headers: {
+                  'Content-Type': 'application/json',
+                  credentials: "include"
+              },
+              body: JSON.stringify({ auth_token: authToken })
+          });
+
+          if (!response.ok) {
+              if(response.status === 500) {
+                  throw new Error('Server error, probeer het later opnieuw');
+              }
+              throw new Error('Error met het authenticeren, probeer het later opniew');
+          }
+
+          if (response.ok) {
+          
+             let data = await response.json();
+             let role = data.role;
+             let id = data.user_id;
+             setUserId(id);
+             if(role != 1) {
+                 throw new Error('Geen toegang tot deze pagina (rol)');
+             }
+             else if(patientID != id){
+              throw new Error('Geen toegang tot deze pagina (id)');
+             }
+          }
+      } catch (error) {
+          console.error('Auth error:', error);
+          navigate('/');
+      }
+  }
+  GetAccountInfo();
     const fetchUser = async (userId) => {
       try {
-        const response = await fetch(`http://localhost:5000/get_user/${userId}`);
+        const response = await fetch(`http://localhost:5000/get_user/${UserId}`);
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -58,7 +100,7 @@ const Settings = ({ userId }) => {
 
   const handleSaveProfile = async () => {
     try {
-      const response = await axios.post(`http://localhost:5000/update_user/3`, {
+      const response = await axios.post(`http://localhost:5000/update_user/${UserId}`, {
         Name: profileInfo.name,
         Lastname: profileInfo.lastname,
         Gender: profileInfo.gender,
@@ -81,190 +123,166 @@ const Settings = ({ userId }) => {
 
   return (
     <div className={`content accessibility-mode-${accessibilityMode}`}>
-      <Navbar />
-      <div className="container formwidth">
-        <div className="row">
-          <div className="col-md-6">
-            <div className="card mb-4">
-              <div className="card-body">
-                <h3 className="card-title">Profiel Informatie</h3>
-                {isEditingProfile ? (
-                  <div>
-                    <div className="mb-3">
-                      <label htmlFor="name" className="form-label">Voornaam:</label>
-                      <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        className="form-control"
-                        value={profileInfo.name}
-                        onChange={handleInputChange}
-                      />
-                    </div>
-                    <div className="mb-3">
-                      <label htmlFor="lastname" className="form-label">Achternaam:</label>
-                      <input
-                        type="text"
-                        id="lastname"
-                        name="lastname"
-                        className="form-control"
-                        value={profileInfo.lastname}
-                        onChange={handleInputChange}
-                      />
-                    </div>
-                    <div className="mb-3">
-                      <label htmlFor="gender" className="form-label">Geslacht:</label>
-                      <input
-                        type="text"
-                        id="gender"
-                        name="gender"
-                        className="form-control"
-                        value={profileInfo.gender}
-                        onChange={handleInputChange}
-                      />
-                    </div>
-                    <div className="mb-3">
-                      <label htmlFor="email" className="form-label">Email:</label>
-                      <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="form-control"
-                        value={profileInfo.email}
-                        onChange={handleInputChange}
-                      />
-                    </div>
-                    <div className="mb-3">
-                      <label htmlFor="phone" className="form-label">Telefoonnummer:</label>
-                      <input
-                        type="tel"
-                        id="phone"
-                        name="phone"
-                        className="form-control"
-                        value={profileInfo.phone}
-                        onChange={handleInputChange}
-                      />
-                    </div>
-                    <button className="btn btn-outline-primary me-2" onClick={handleCancelEdit}>Cancel</button>
-                    <button className="btn btn-outline-primary" onClick={handleSaveProfile}>Accepteer</button>
-                  </div>
-                ) : (
-                  <div>
-                    <p>Voornaam: {profileInfo.name}</p>
-                    <p>Achternaam: {profileInfo.lastname}</p>
-                    <p>Geslacht: {profileInfo.gender}</p>
-                    <p>Email: {profileInfo.email}</p>
-                    <p>Telefoonnummer: {profileInfo.phone}</p>
-                    <button className="btn btn-outline-primary" onClick={handleEditProfile}>Verander Profiel Informatie</button>
-                  </div>
-                )}
+      <Navbar/>
+      <div className="container">
+        <div className="setting-option">
+          <h3>Profiel Informatie</h3>
+          {isEditingProfile ? (
+            <div>
+              <div>
+                <label htmlFor="name">Voornaam:</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={profileInfo.name}
+                  onChange={handleInputChange}
+                />
               </div>
+              <div>
+                <label htmlFor="lastname">Achternaam:</label>
+                <input
+                  type="text"
+                  id="lastname"
+                  name="lastname"
+                  value={profileInfo.lastname}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="gender">Geslacht:</label>
+                <input
+                  type="text"
+                  id="gender"
+                  name="gender"
+                  value={profileInfo.gender}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={profileInfo.email}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="phone">Telefoonnummer:</label>
+                <input
+                  type="tel"
+                  id="phone"
+                  name="phone"
+                  value={profileInfo.phone}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <button onClick={handleCancelEdit}>Cancel</button>
+              <button onClick={handleSaveProfile}>Accepteer</button>
             </div>
+          ) : (
+            <div>
+              <p>Voornaam: {profileInfo.name}</p>
+              <p>Achternaam: {profileInfo.lastname}</p>
+              <p>Geslacht: {profileInfo.gender}</p>
+              <p>Email: {profileInfo.email}</p>
+              <p>Telefoonnummer: {profileInfo.phone}</p>
+              <button onClick={handleEditProfile}>Verander Profiel Informatie</button>
+            </div>
+          )}
+        </div>
+        <div className="settings-options">
+          <h3>Toegankelijkheids Opties</h3>
+          <div>
+            <input 
+              type="radio" 
+              id="normal" 
+              name="accessibility" 
+              value="1" 
+              checked={accessibilityMode === 1} 
+              onChange={() => setAccessibilityMode(1)} 
+            />
+            <label htmlFor="normal">Standaard Modus</label>
           </div>
-          <div className="col-md-6">
-            <div className="card mb-4">
-              <div className="card-body">
-                <h3 className="card-title">Toegankelijkheids Opties</h3>
-                <div className="form-check">
-                  <input 
-                    type="radio" 
-                    id="normal" 
-                    name="accessibility" 
-                    value="1" 
-                    className="form-check-input"
-                    checked={accessibilityMode === 1} 
-                    onChange={() => setAccessibilityMode(1)} 
-                  />
-                  <label htmlFor="normal" className="form-check-label">Standaard Modus</label>
-                </div>
-                <div className="form-check">
-                  <input 
-                    type="radio" 
-                    id="dark" 
-                    name="accessibility" 
-                    value="2" 
-                    className="form-check-input"
-                    checked={accessibilityMode === 2} 
-                    onChange={() => setAccessibilityMode(2)} 
-                  />
-                  <label htmlFor="dark" className="form-check-label">Donkere Modus</label>
-                </div>
-                <div className="form-check">
-                  <input
-                    type="radio"
-                    id="protanopia_deuteranopia"
-                    name="accessibility"
-                    value="3"
-                    className="form-check-input"
-                    checked={accessibilityMode === 3}
-                    onChange={() => setAccessibilityMode(3)}
-                  />
-                  <label htmlFor="protanopia_deuteranopia" className="form-check-label">Protanopie en Deuteranopie</label>
-                </div>
-                <div className="form-check">
-                  <input
-                    type="radio"
-                    id="tritanopia"
-                    name="accessibility"
-                    value="4"
-                    className="form-check-input"
-                    checked={accessibilityMode === 4}
-                    onChange={() => setAccessibilityMode(4)}
-                  />
-                  <label htmlFor="tritanopia" className="form-check-label">Tritanopie</label>
-                </div>
-                <div className="form-check">
-                  <input
-                    type="radio"
-                    id="achromatopsia"
-                    name="accessibility"
-                    value="5"
-                    className="form-check-input"
-                    checked={accessibilityMode === 5}
-                    onChange={() => setAccessibilityMode(5)}
-                  />
-                  <label htmlFor="achromatopsia" className="form-check-label">Achromatopsie</label>
-                </div>
-                <div className="form-check">
-                  <input
-                    type="radio"
-                    id="achromatomaly"
-                    name="accessibility"
-                    value="6"
-                    className="form-check-input"
-                    checked={accessibilityMode === 6}
-                    onChange={() => setAccessibilityMode(6)}
-                  />
-                  <label htmlFor="achromatomaly" className="form-check-label">Achromatomaly</label>
-                </div>
-                <div className="form-check">
-                  <input
-                    type="radio"
-                    id="high_contrast"
-                    name="accessibility"
-                    value="7"
-                    className="form-check-input"
-                    checked={accessibilityMode === 7}
-                    onChange={() => setAccessibilityMode(7)}
-                  />
-                  <label htmlFor="high_contrast" className="form-check-label">Hoge Contrast Modus</label>
-                </div>
-                <div className="form-check">
-                  <input
-                    type="checkbox"
-                    id="emailNotifications"
-                    className="form-check-input"
-                    checked={emailNotifications}
-                    onChange={() => setEmailNotifications(!emailNotifications)}
-                  />
-                  <label htmlFor="emailNotifications" className="form-check-label">Email Notificaties</label>
-                </div>
-                <button className="btn btn-outline-primary mt-2" onClick={handleSaveProfile}>Opties opslaan</button>
-              </div>
-            </div>
+          <div>
+            <input 
+              type="radio" 
+              id="dark" 
+              name="accessibility" 
+              value="2" 
+              checked={accessibilityMode === 2} 
+              onChange={() => setAccessibilityMode(2)} 
+            />
+            <label htmlFor="dark">Donkere Modus</label>
+          </div>
+          {/* Add additional accessibility options here with respective values */}
+          <div>
+            <input
+              type="radio"
+              id="protanopia_deuteranopia"
+              name="accessibility"
+              value="3"
+              checked={accessibilityMode === 3}
+              onChange={() => setAccessibilityMode(3)}
+            />
+            <label htmlFor="protanopia_deuteranopia">Protanopie en Deuteranopie</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="tritanopia"
+              name="accessibility"
+              value="3"
+              checked={accessibilityMode === 4}
+              onChange={() => setAccessibilityMode(4)}
+            />
+            <label htmlFor="tritanopia">Tritanopie</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="achromatopsia"
+              name="accessibility"
+              value="4"
+              checked={accessibilityMode === 5}
+              onChange={() => setAccessibilityMode(5)}
+            />
+            <label htmlFor="achromatopsia">Achromatopsie</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="achromatomaly"
+              name="accessibility"
+              value="5"
+              checked={accessibilityMode === 6}
+              onChange={() => setAccessibilityMode(6)}
+            />
+            <label htmlFor="achromatomaly">Achromatomaly</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="high_contrast"
+              name="accessibility"
+              value="6"
+              checked={accessibilityMode === 7}
+              onChange={() => setAccessibilityMode(7)}
+            />
+            <label htmlFor="high_contrast">Hoge Contrast Modus</label>
           </div>
         </div>
-
+        <div className="settings-option">
+          <input
+            type="checkbox"
+            id="emailNotifications"
+            checked={emailNotifications}
+            onChange={() => setEmailNotifications(!emailNotifications)}
+          />
+          <label htmlFor="emailNotifications">Email Notificaties</label>
+        </div>
       </div>
     </div>
   );
